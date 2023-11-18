82299cb66393c1094da16be61f354305f723905f
HoangQuan74 <quan.h.nguyen@tgl-sol.com>
2023-11-18 21:58:17 +0700

    fix

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,14 +1,17 @@
 import * as express from 'express';
 const router = express.Router();
 import { userController } from './controllers/userController';
+import Auth from 'src/common/helpers/auth';
 const schoolController = require('./controllers/schoolController');
+
+const authInstance = new Auth();
 
 router.post('/login', userController.login);
 
 router.get('/academic-year', schoolController.getAcademicYear);
 router.get('/semester', schoolController.getSemester);
 
-router.get('/profile', userController.getProfile);
+router.get('/profile', authInstance.auth ,userController.getProfile);
 
 
 module.exports = router;
