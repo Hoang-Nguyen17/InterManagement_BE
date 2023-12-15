870ddca7038bc588b65e64f8096807bff9cafbc8
HoangQuan74 <quan.h.nguyen@tgl-sol.com>
2023-12-16 02:35:15 +0700

    fix login

--- src/api/user/controllers/userController.ts
+++ src/api/user/controllers/userController.ts
@@ -17,7 +17,7 @@ const login = async (req: Request, res: Response) => {
         const { username, pass } = value;
         const us = new UserService();
         const { result, schoolId = 0 } = await us.login(username, pass);
-        if (!(result && (result.permission_id === PermissionConstants.MANAGE_APP || schoolId))) 
+        if (!(result && (result.permission_id === PermissionConstants.MANAGE_APP || result.permission_id === PermissionConstants.BUSINESS || schoolId)))
             return res.status(400).json({ detail: 'Đăng nhập thất bại, không tìm thấy tài khoản' });
 
         delete result.pass;
