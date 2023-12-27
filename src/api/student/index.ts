5203ec70add4c40d95a862a43e451ef578e76043
Annoy1010 <lethephuc2002@gmail.com>
2023-12-28 03:36:10 +0700

    Fix wrong API get registed intern subject

--- src/api/student/index.ts
+++ src/api/student/index.ts
@@ -14,7 +14,7 @@ router.post('/introducing-letter', authInstance.auth, StudentRequestRegistIntern
 router.delete('/introducing-letter/:id', authInstance.auth, StudentRequestRegistIntern.deleteRequest);
 
 // student Learn Intern
-router.get('/intern-subject/:id/learn-intern', authInstance.auth, InternSubjectController.learnInternDetail)
+router.get('/intern-subject/learn-intern', authInstance.auth, InternSubjectController.learnInternDetail)
 router.post('/intern-subject/:id/learn-intern', authInstance.auth, InternSubjectController.saveLearnIntern);
 router.delete('/intern-subject/:id/learn-intern/:lid', authInstance.auth, InternSubjectController.deleteLearnIntern);
 
